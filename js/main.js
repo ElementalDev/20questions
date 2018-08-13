9da41b1bb4f3f948674634a0cc655d7196df074f
Reece_Collett <rcollett@spartaglobal.com>
2018-08-13 17:59:43 +0100

    Removed seperate page. Now using single page while hiding certain elements

--- js/main.js
+++ js/main.js
@@ -1,34 +1,38 @@
 $(function() {
 
+  //Hide other screens
+  $("#questionsScreen").hide();
+
   $("#startBtn").click(function (){
-    window.location = "quickfirequestions.html";
+    var diff = getDifficulty();
+    var questions = getQuestions(diff);
+    $("#titleScreen").hide();
+    $("#questionsScreen").show();
   })
 
+  function getDifficulty() {
+    return $('.diffBtns:checked').val();
+  }
+
   function getQuestions(difficulty) {
-    //Variable to store the questions
-    var questions;
-    //If questions are a certain difficulty, assign them to questions
     switch (difficulty) {
+
       case "easy":
         $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple", function(data){
-          questions = data;
+          return data;
         })
         break;
       case "medium":
         $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple", function(data){
-          questions = data;
-          debugger;
+          return data;
+
         })
         break;
       case "hard":
         $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=hard&type=multiple", function(data){
-          questions = data;
+          return data;
         })
         break;
-      default: throw alert("This is not a difficulty");
     }
   }
-
-  getDifficulty();
-
 })
