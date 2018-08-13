eafb92febf0bb4412aac7159915c161f1f86ad16
Reece_Collett <rcollett@spartaglobal.com>
2018-08-13 17:13:12 +0100

    Added getQuestions function that gets questions via difficulty from an API

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
 $(function() {
+
   $("#startBtn").click(function (){
     window.location = "quickfirequestions.html";
   })
 
-  function questionGet(difficulty) {
+  function getQuestions(difficulty) {
+    //Variable to store the questions
+    var questions;
+    //If questions are a certain difficulty, assign them to questions
     switch (difficulty) {
       case "easy":
-        setInterval(function(){
-          $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple", function(data){
-            
-          })
-        }, 10000)
+        $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple", function(data){
+          questions = data;
+        })
         break;
+      case "medium":
+        $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple", function(data){
+          questions = data;
+          debugger;
+        })
+        break;
+      case "hard":
+        $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=hard&type=multiple", function(data){
+          questions = data;
+        })
+        break;
+      default: throw alert("This is not a difficulty");
     }
   }
 
-  questionGet("easy");
+  getDifficulty();
+
 })
