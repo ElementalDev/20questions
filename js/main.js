5b8a5e3994655317214f15aad8ff9e33793605d6
Reece_Collett <rcollett@spartaglobal.com>
2018-08-13 16:53:55 +0100

    Added AJAX with a quiz api

--- js/main.js
+++ js/main.js
@@ -2,4 +2,18 @@ $(function() {
   $("#startBtn").click(function (){
     window.location = "quickfirequestions.html";
   })
+
+  function questionGet(difficulty) {
+    switch (difficulty) {
+      case "easy":
+        setInterval(function(){
+          $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple", function(data){
+            
+          })
+        }, 10000)
+        break;
+    }
+  }
+
+  questionGet("easy");
 })
