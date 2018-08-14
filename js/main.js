90dbb6060a4ff2eb12e7a5d6e2c79ebf1c3f856a
Reece_Collett <rcollett@spartaglobal.com>
2018-08-14 11:55:46 +0100

    Question displays on screen

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 $(function() {
 
-  //Hide other screens
-  $("#questionsScreen").hide();
+  var diff;
+  var questions;
 
-  //When the user clicks the start button
-  $("#startBtn").click(function (){
-    var diff = getDifficulty();
-    var questions = getQuestions(diff);
-    $("#titleScreen").hide();
-    $("#questionsScreen").show();
-  })
+  //Hide other screens
+  $("#questionsScreenP1").hide();
+  $("#questionsScreenP2").hide();
+  $("#questions1").hide();
+  $("#questions2").hide();
 
   //Get and return the difficulty from the radio buttons
   function getDifficulty() {
@@ -19,23 +17,66 @@ $(function() {
   //get questions from an API
   function getQuestions(difficulty) {
     switch (difficulty) {
-
       case "easy":
-        $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple", function(data){
-          return data;
+          $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple", function(data){
+          questions = data;
+          return questions;
         })
         break;
       case "medium":
-        $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple", function(data){
-          return data;
-
+          $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple", function(data){
+          questions = data;
+          return questions;
         })
         break;
       case "hard":
         $.get("https://opentdb.com/api.php?amount=50&category=9&difficulty=hard&type=multiple", function(data){
-          return data;
+          questions = data;
+          return questions;
         })
         break;
     }
   }
+
+  //Show the questions
+  function showQuestions(ques) {
+    var quesNumber = 0;
+    var answerBtns = $(".answerBtns");
+    var randQues = Math.floor(Math.random() * 50);
+    var randBtn = Math.floor(Math.random() * 4) + 1;
+    //Display the first question
+    $("#question").html(ques.results[randQues].question);
+    $("#answer" + randBtn).html("hello");
+
+
+    // Do this function every 10 seconds
+    var timer = setInterval(function() {
+      $(".answerBtns").html("");
+      //Create a new random number every time
+      randQues = Math.floor(Math.random() * 50);
+      randBtn = Math.floor(Math.random() * 4) + 1;
+      quesNumber++
+      if (quesNumber >= 9) {
+        //Stop the timer
+        clearInterval(timer);
+        alert("Finished!");
+      }
+      //Display the question
+      $("#question").html(ques.results[randQues].question);
+      $("#answer" + randBtn).html("hello");
+    }, 2000);
+  }
+
+  //When the user clicks the start button
+  $("#startBtn").click(function (){
+    diff = getDifficulty();
+    questions = getQuestions(diff);
+    $("#titleScreen").hide();
+    $("#questionsScreenP1").show()
+  })
+  $("#startQues").click(function(){
+    $(this).hide();
+    $("#questions1").show();
+    showQuestions(questions);
+  })
 })
